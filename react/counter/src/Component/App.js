96036e2d91098e202a41849466827fc416174414
Somenae <kevindevaux@neuf.fr>
2024-01-04 13:01:55 +0100

    counter done

--- react/counter/src/Component/App.js
+++ react/counter/src/Component/App.js
@@ -1,9 +1,72 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Counter from "./Counter";
 
-function App(title) {
+function App({ title }) {
+  // Un seul state doit gérer 5 compteurs
+  const [counters, setCounters] = useState([
+    {value:0, id:0},
+    {value:1, id:1},
+    {value:2, id:2},
+    {value:3, id:3},
+    {value:4, id:4},
+  ]);
+
+  function handleCLickLess(counterId) {
+    // Copie du state
+    const countersCopy = [...counters];
+    countersCopy.forEach(counter => {
+      if (counterId === counter.id) {
+        counter.value --;
+      }
+    });
+    // Changement du state
+    setCounters(() => countersCopy);
+  }
+
+  function handleClickMore(counterId) {
+    // Copie du state
+    const countersCopy = [...counters];
+    countersCopy.forEach(counter => {
+      if (counterId === counter.id) {
+        counter.value ++;
+      }
+    });
+    // Changement du state
+    setCounters(() => countersCopy);
+  }
+
+  function handleCLickLessAll() {
+    // Copie du state
+    const countersCopy = [...counters];
+    countersCopy.forEach(counter => {
+      counter.value --;
+    });
+    // Changement du state
+    setCounters(() => countersCopy);
+  }
+
+  function handleCLickMoreAll() {
+        // Copie du state
+        const countersCopy = [...counters];
+        countersCopy.forEach(counter => {
+          counter.value ++;
+        });
+        // Changement du state
+        setCounters(() => countersCopy);
+  }
+
   return (
     <div className="App">
-      <h1>{title.title}</h1>
+      <h1>{title}</h1>
+      {counters.map((counter) => <Counter 
+      key = { counter.id } 
+      counter = { counter } 
+      onCLickLess={handleCLickLess}
+      onCLickMore={handleClickMore}
+      onCLickLessAll={handleCLickLessAll}
+      onCLickMoreAll={handleCLickMoreAll} />)}
+      {/* Appel du composant fonction Counter */}
     </div>
   );
 }
--- react/counter/src/index.js
+++ react/counter/src/index.js
@@ -6,6 +6,6 @@ import App from './Component/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App title = {"Compteurs"} />
+    <App title = {"Compteurs"} age={35} />
   </React.StrictMode>
 );
